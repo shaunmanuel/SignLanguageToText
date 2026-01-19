16709bf57d1b96a2ba201129fdaa2e11ea61f539
shaun s manuel <shaunsmanuel@shauns-MacBook-Air.local>
2026-01-19 21:13:49 +0530

    Update data collection and model improvements

--- src/live_prediction.py
+++ src/live_prediction.py
@@ -10,7 +10,7 @@ from collections import deque
 model = tf.keras.models.load_model("../model/gesture_model.h5")
 
 # Label mapping (must match training order)
-labels = ["A", "B", "C","H","I"]
+labels = ["A", "B", "C","H","I","D","E","F"]
 
 # ==============================
 # MEDIAPIPE HANDS SETUP
